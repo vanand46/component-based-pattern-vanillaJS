ccb2b98c25c7a8cb6bdc7831580b09814fe590c0
Anand V <42205127+vanand46@users.noreply.github.com>
2024-10-12 17:34:48 +0530

    Initial Upload

---
+++ src/pages/detail.js
@@ -0,0 +1,19 @@
+import { createHeader } from "../components/header.js";
+
+const rootDiv = document.getElementById("root");
+
+const header = createHeader();
+
+const bodyContainer = document.createElement("div");
+const pageTitle = document.createElement("h1");
+pageTitle.innerHTML = "This is detail page";
+bodyContainer.appendChild(pageTitle);
+bodyContainer.style.cssText = `
+    display: flex;
+    justify-content: center;
+    height: 60vh;
+    align-items: center;
+`;
+
+rootDiv.appendChild(header);
+rootDiv.appendChild(bodyContainer);
